48826b091f5175ba914d4012f3c6a3deaae226f8
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-16 17:24:50 +0400

    Change example status to out of maintenance

--- CS/DXPivotGrid_CustomContextMenu/CustomCommands.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows.Input;
-
-namespace CustomContextMenu {
-    public static class CustomCommands {
-        static RoutedUICommand copy = new RoutedUICommand("Copy", "Copy", typeof(CustomCommands));
-        public static RoutedUICommand Copy { get { return copy; } }
-    }
-}
